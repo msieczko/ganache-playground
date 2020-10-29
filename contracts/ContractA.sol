1553cff89960d3c897527357f6ffa5e08f49be49
Michał Sieczkowski <michal@ethworks.io>
2020-10-29 14:35:21 +0100

    Remove contracts

--- contracts/ContractA.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.6.0;
-
-contract ContractA {
-    string public name;
-    constructor() public {
-        name = "A";
-    }
-}
--- contracts/ContractB.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.6.0;
-
-contract ContractB {
-    string public name;
-    constructor() public {
-        name = "B";
-    }
-}
