c7d5bcd97417f19de1dda982c5d3b4a2e6e0dc62
Michał Sieczkowski <michal@ethworks.io>
2020-10-29 13:10:44 +0100

    Initial commit

---
+++ contracts/ContractA.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.6.0;
+
+contract ContractA {
+    string public name;
+    constructor() public {
+        name = "A";
+    }
+}
---
+++ test/ganache.ts
@@ -0,0 +1,13 @@
+import {MockProvider} from "ethereum-waffle"
+
+describe('Ganache', () => {
+  let provider: MockProvider
+
+  beforeEach(() => {
+    provider = new MockProvider()
+  })
+
+  it('works', async () => {
+
+  })
+})
