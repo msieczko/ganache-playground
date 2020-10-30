0025164936191929c87feb321d8c44ae6a37d7a2
Michał Sieczkowski <michal@ethworks.io>
2020-10-30 13:13:03 +0100

    Update tests

--- test/ganache.test.ts
+++ test/ganache.test.ts
@@ -1,9 +1,10 @@
-import {MockProvider} from "ethereum-waffle"
+import {MockProvider, solidity} from "ethereum-waffle"
 import {constants, Wallet} from "ethers"
 import chai, {expect} from "chai"
 import chaiAsPromised from 'chai-as-promised'
 
 chai.use(chaiAsPromised)
+chai.use(solidity)
 
 describe('Ganache', () => {
   let provider: MockProvider
@@ -17,14 +18,13 @@ describe('Ganache', () => {
   })
 
   afterEach(async () => {
-    await sleep(1_000)
     provider.off("block", printBlock)
   })
 
   function printBlock(blockNumber: number) {
     provider.getBlock(blockNumber).then(block => {
-      console.log(`Block #${block.number}`)
-      console.log(`Transactions = [${block.transactions.join(", ")}]\n`)
+      console.log(`\nBlock #${block.number}`)
+      console.log(`Transactions = [${block.transactions.join(", ")}]`)
     })
   }
 
@@ -50,6 +50,19 @@ describe('Ganache', () => {
     })
 
     await sleep(4_000)
+
+    // RESULT:
+    // Block #0
+    // Transactions = []
+    //
+    // Block #1
+    // Transactions = [0x64f70d7baed73fc87f116ba1e944fc0da6132c5812b9c0185c4d98f5df2a62ba, 0x4d2f3ad5434b328c9b24ad69b1ec22b37f84d3b09f0197d5bff84271f0932a48, 0xb41d2c28841353389dd9e6eda69ce3e51e1c705acd8e7b9d749c132d3d6ed0e9]
+    //
+    // Block #2
+    // Transactions = []
+    //
+    // Block #3
+    // Transactions = []
   }).timeout(0)
 
   it('separates the transactions into two blocks if they exceed block gas limit', async () => {
@@ -70,6 +83,19 @@ describe('Ganache', () => {
     })
 
     await sleep(4_000)
+
+    // RESULT:
+    // Block #0
+    // Transactions = []
+    //
+    // Block #1
+    // Transactions = [0x64f70d7baed73fc87f116ba1e944fc0da6132c5812b9c0185c4d98f5df2a62ba, 0xc8f2f098d58526fa2bceda6a8e14990cd1d7b7e25b31b8094e11a8e27e693d3f]
+    //
+    // Block #2
+    // Transactions = [0xb41d2c28841353389dd9e6eda69ce3e51e1c705acd8e7b9d749c132d3d6ed0e9]
+    //
+    // Block #3
+    // Transactions = []
   }).timeout(0)
 
   it('does not handle transaction queueing', async () => {
@@ -108,5 +134,29 @@ describe('Ganache', () => {
     })
 
     await sleep(4_000)
+
+    // RESULT:
+    // Block #0
+    // Transactions = []
+    //
+    // Block #1
+    // Transactions = []
+    //
+    // Block #2
+    // Transactions = []
+  }).timeout(0)
+
+  it("does not allow to call eth_getBalance in the context of 'pending' block", async () => {
+    const initialBalance = await walletB.getBalance()
+    await walletA.sendTransaction({
+      to: walletB.address,
+      nonce: 0,
+      value: 100
+    })
+    await sleep(500)
+    const balance = await provider.getBalance(walletB.address, 'pending')
+    await expect(provider.getBalance(walletB.address, 1))
+      .to.be.rejectedWith("'blocks' index out of range: index 1; length: 1")
+    expect(balance).to.not.eq(initialBalance.add(100))
   }).timeout(0)
 })
